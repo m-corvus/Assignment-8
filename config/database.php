f2d34564735c2106e675be69bdbefdb422a0f7a8
akmal <satoefrekuensi@gmail.com>
2021-10-18 20:14:36 +0700

    database

--- config/database.php
+++ config/database.php
@@ -49,7 +49,7 @@ return [
             'port' => env('DB_PORT', '3306'),
             'database' => env('DB_DATABASE', 'deploy_heroku'),
             'username' => env('DB_USERNAME', 'admindeploy'),
-            'password' => env('DB_PASSWORD', 'AdminDeploy2021'),
+            'password' => env('DB_PASSWORD', 'Admindeploy2021'),
             'unix_socket' => env('DB_SOCKET', ''),
             'charset' => 'utf8mb4',
             'collation' => 'utf8mb4_unicode_ci',
